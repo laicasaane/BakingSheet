ab96fb1df10ffa02ca61fedad6fdde00625487b5
Laicasaane <laicasaane@outlook.com>
2026-06-29 10:51:21 +0700

    Change package folder name to com.laicasaane.bakingsheet

--- copy.ps1
+++ copy.ps1
@@ -1,6 +1,6 @@
-cp -R -force BakingSheet/Src/* UnityProject/Packages/com.cathei.bakingsheet/Runtime/Core
-cp BakingSheet.Converters.Excel/*.cs UnityProject/Packages/com.cathei.bakingsheet/Runtime/Converters/Excel
-cp BakingSheet.Converters.Google/*.cs UnityProject/Packages/com.cathei.bakingsheet/Runtime/Converters/Google
-cp BakingSheet.Converters.Csv/*.cs UnityProject/Packages/com.cathei.bakingsheet/Runtime/Converters/Csv
-cp BakingSheet.Converters.Json/*.cs UnityProject/Packages/com.cathei.bakingsheet/Runtime/Converters/Json
-cp *.md UnityProject/Packages/com.cathei.bakingsheet/
+cp -R -force BakingSheet/Src/* UnityProject/Packages/com.laicasaane.bakingsheet/Runtime/Core
+cp BakingSheet.Converters.Excel/*.cs UnityProject/Packages/com.laicasaane.bakingsheet/Runtime/Converters/Excel
+cp BakingSheet.Converters.Google/*.cs UnityProject/Packages/com.laicasaane.bakingsheet/Runtime/Converters/Google
+cp BakingSheet.Converters.Csv/*.cs UnityProject/Packages/com.laicasaane.bakingsheet/Runtime/Converters/Csv
+cp BakingSheet.Converters.Json/*.cs UnityProject/Packages/com.laicasaane.bakingsheet/Runtime/Converters/Json
+cp *.md UnityProject/Packages/com.laicasaane.bakingsheet/
