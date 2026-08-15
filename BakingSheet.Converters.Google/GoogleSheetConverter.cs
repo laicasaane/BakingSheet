5d3e7dfbd1e27bd2d2d973a488e4a654e5692d89
Laicasaane <laicasaane@outlook.com>
2026-08-15 22:34:48 +0700

    Remove stale internal namespace imports

--- BakingSheet.Converters.Google/GoogleSheetConverter.cs
+++ BakingSheet.Converters.Google/GoogleSheetConverter.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Cathei.BakingSheet.Internal;
 using Cathei.BakingSheet.Raw;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
--- BakingSheet/Src/SheetContainerBase.cs
+++ BakingSheet/Src/SheetContainerBase.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Cathei.BakingSheet.Internal;
 using Microsoft.Extensions.Logging;
 
 namespace Cathei.BakingSheet
